224d7c84d83413b1de5ea04161cb9636529b15b5
gatwi <faith.murithi@student.moringaschool.com>
2022-07-12 15:32:17 +0300

    save

---
+++ src/Page.js
@@ -0,0 +1,32 @@
+
+import React from "react";
+
+// some app variables
+const title = "JavaScript"
+const image = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png"
+const features = [
+    'Light Weight Scripting language.',
+    'Dynamic Typing.',
+    'Object-oriented programming support.',
+    'Functional Style.',
+    'Platform Independent.',
+]
+
+// jsx
+const JSXItem = <input type="submit" className={'button'}/>
+const OtherJSX = <div>{JSXItem}</div>
+
+function Homepage(){
+    return (
+        <div>
+            <h4>Title</h4>
+            <h1>{title}</h1>
+            <h4>Image</h4>
+            <img src={image}/>
+            <h4>Features</h4>
+            {features.map((element) => <p>{element}</p>)}
+        </div>
+    )
+}
+
+export default Homepage
